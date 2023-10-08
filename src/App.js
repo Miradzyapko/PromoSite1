049208116e6a38f1c3d483d0b4405135f171406a
Miradzyapko <mira.dzyapko@gmail.com>
2023-10-08 18:09:30 +0300

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App1">
+    <div className="App2">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
